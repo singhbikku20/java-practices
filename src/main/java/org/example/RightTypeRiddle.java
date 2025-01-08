7d960ca1dfc0983b291f51833e8ae94df4a27acc
singhbikku20 <singhbikku20@gmail.com>
2025-01-08 16:25:57 +0530

    Greetings, Lopping and DataType riddle programs

---
+++ src/main/java/org/example/RightTypeRiddle.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class RightTypeRiddle {
+    public static void main(String[] args) {
+        byte a = 1;
+        short b = 2;
+        int c = 3;
+        long d = 4;
+        float e = 5.0f;
+        double f = 6.0;
+        char g = 'A';
+        String h = "Hello";
+        boolean i = true;
+
+        System.out.println(a+" is an "+((Object)a).getClass().getSimpleName());
+        System.out.println(b+" is an "+((Object)b).getClass().getSimpleName());
+        System.out.println(c+" is an "+((Object)c).getClass().getSimpleName());
+        System.out.println(d+" is an "+((Object)d).getClass().getSimpleName());
+        System.out.println(e+" is an "+((Object)e).getClass().getSimpleName());
+        System.out.println(f+" is an "+((Object)f).getClass().getSimpleName());
+        System.out.println(g+" is an "+((Object)g).getClass().getSimpleName());
+        System.out.println(h+" is an "+((Object)h).getClass().getSimpleName());
+        System.out.println(i+" is an "+((Object)i).getClass().getSimpleName());
+
+
+    }
+}
