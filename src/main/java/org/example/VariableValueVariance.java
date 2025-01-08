74782f7cd07b3797f4fef9d5e54dbddd827e7306
singhbikku20 <singhbikku20@gmail.com>
2025-01-08 17:50:18 +0530

    Variable Voyage,variance & Data type detection

---
+++ src/main/java/org/example/VariableValueVariance.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class VariableValueVariance {
+
+    public static void main (String[] args) {
+        int a = 5;
+        System.out.println("Value of a is: " + a);
+        a=7;
+        System.out.println("Value of a after change is: " + a);
+    }
+}
---
+++ src/main/java/org/example/VariableVoyage.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class VariableVoyage {
+
+    public static void main(String[] args){
+
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Sum of "+a+" and "+b+" is: " + (a + b));
+
+    }
+}
