86be08f77678d4d3f7bde8682189e58241a22b1b
singhbikku20 <singhbikku20@gmail.com>
2025-01-09 17:39:31 +0530

    CastingConnoisseur program

---
+++ src/main/java/org/controlStructandBasicOps/CastingConnoisseur.java
@@ -0,0 +1,19 @@
+package org.controlStructandBasicOps;
+
+public class CastingConnoisseur {
+
+    public static void main(String[] args){
+
+        double a = 5.7;
+        int b   = 2;
+        int c   = (int) a;
+        double d = b;
+
+        System.out.println("Double a: " + a);
+        System.out.println("Int b: " + b);
+        System.out.println("Converted a: " + c);
+        System.out.println("Converted b: " + d);
+
+    }
+
+}
